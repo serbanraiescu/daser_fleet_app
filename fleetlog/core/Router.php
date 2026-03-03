0384e5fa1d89c0ed048cd779e8b0e812fc784be0
serbanraiescu <serbanraiescu@gmail.com>
2026-03-03 11:27:17 +0200

    Cleanup diagnostic logs and finalize archiving fix

--- fleetlog/core/Router.php
+++ fleetlog/core/Router.php
@@ -22,8 +22,6 @@ class Router
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
 
-        error_log("Router Dispatch: $method $uri");
-
         // Robust URI cleaning
         $scriptName = $_SERVER['SCRIPT_NAME'];
         $scriptDir = str_replace('\\', '/', dirname($scriptName));
