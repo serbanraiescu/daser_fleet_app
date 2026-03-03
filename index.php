5e69bdfd71c0b27ccc67e28ed56d2996d7afa533
serbanraiescu <serbanraiescu@gmail.com>
2026-03-03 18:30:54 +0200

    Fix JSON response corruption by removing debug echo

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "<!-- DEBUG: ROOT INDEX.PHP IS RUNNING -->";
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
